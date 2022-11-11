96a91687f9200044e3ca3f1e5e678b1750e3124a
yosh <yosh-git@riseup.net>
2022-11-10 19:07:34 -0500

    layout variables, window title

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Application(tk.Frame):
         tk.Frame.__init__(self, master,
                           width=layout['width'],
                           height=layout['height'])
-        self['bg'] = '#000066'
+        self['bg'] = layout['rootbg']
         self.pack()
         self.create_widgets()
         proc = subprocess.Popen(['xinput', 'test-xi2', '--root'],
@@ -35,9 +35,11 @@ class Application(tk.Frame):
                         code = int(line.split()[1])
                         try:
                             if inkeypressevent == True:
-                                buttons[code]['bg'] = '#FFFFFF'
+                                buttons[code]['bg'] = layout['bg2']
+                                buttons[code]['fg'] = layout['fg2']
                             elif inkeyrelevent == True:
-                                buttons[code]['bg'] = '#777777'
+                                buttons[code]['bg'] = layout['bg1']
+                                buttons[code]['fg'] = layout['fg1']
                         except KeyError:
                             pass
                     inkeypressevent = False
@@ -51,11 +53,13 @@ class Application(tk.Frame):
             btn = tk.Label(text=button['text'])
             btn.place(width=button['width'], height=button['height'],
                       x=button['x'], y=button['y'])
-            btn['bg'] = '#777777'
-            btn['font'] = (None, 12)
+            btn['bg'] = layout['bg1']
+            btn['fg'] = layout['fg1']
+            btn['font'] = (layout['fontfamily'], layout['fontsize'], layout['fontweight'])
             buttons[button['keycode']] = btn
             timesran += 1
 
 
 root = tk.Tk()
+root.title("kbdisplay")
 app = Application(master=root)
